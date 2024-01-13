bc21843a80279a46fd8c5c9077b499853541aceb
ArunKashyap <arunbhagat132@gmail.com>
2024-01-13 17:35:26 +0530

    SubString

--- src/oddEVEN/AboutHashMap.java
+++ src/oddEVEN/AboutHashMap.java
@@ -1,73 +1,117 @@
 package oddEVEN;
+
 import java.util.HashMap;
 import java.util.Arrays;
+
 public class AboutHashMap {
 
+    // Some important methods of the hashMaps
+    // 1. containsKey(key) - this method will check whether the key is already
+    // present or not
+    // 2. put(key , value) - this method is used to put the value in the hashmap
+    // example - map.put(i,map.get(i)+1)
+    // 3. get(key) - this method will take an key as argument and it will check the
+    // value of the that key
+    // 4. keySet() - this will give us the set of all the keys in the map so that we
+    // can easily traverse through that
 
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
-System.out.println(Arrays.toString(mergeSort(arr)));        
+
+        int[] arr = {5,4,3,4,3,4,323,234,2,1};
+        int[] ans = mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+
     }
 
-    static int[] mergeSort(int[] arr) {
+  static  int[] mergeSort(int arr[]) {
+        // first we know this when array.length == 1 then we have to return the element
+
         if (arr.length == 1) {
             return arr;
         }
 
-        int mid = arr.length / 2;
+        int mid = arr.length/2;
 
-        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
-        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        int left[] = mergeSort(Arrays.copyOfRange(arr, 0 ,mid));
+        int right[] = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
 
         return merge(left, right);
+
     }
 
-    private static int[] merge(int[] first, int[] second) {
-        int[] mix = new int[first.length + second.length];
+    private static int[] merge(int[] left, int[] right) {
+        int mergedArray[] = new int[left.length + right.length];
 
         int i = 0;
         int j = 0;
         int k = 0;
-
-        while (i < first.length && j < second.length) {
-            if (first[i] < second[j]) {
-                mix[k] = first[i];
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                mergedArray[k] = left[i];
                 i++;
             } else {
-                mix[k] = second[j];
+                mergedArray[k] = right[j];
                 j++;
             }
             k++;
         }
 
-        // it may be possible that one of the arrays is not complete
-        // copy the remaining elements
-        while (i < first.length) {
-            mix[k] = first[i];
+        while (i < left.length) {
+            mergedArray[k] = left[i];
             i++;
             k++;
-        }
 
-        while (j < second.length) {
-            mix[k] = second[j];
+        }
+        while (j < right.length) {
+            mergedArray[k] = right[i];
             j++;
             k++;
+
         }
 
-        return mix;
+        return mergedArray;
     }
 
+    HashMap<Character, Integer> romanToInt = new HashMap<>();
 
-    HashMap<Character,Integer> romanToInt = new HashMap<>();
-    void adddingValues(){
-          romanToInt.put('I',1);
-          romanToInt.put('V',5);
-          romanToInt.put('X',10);
-          romanToInt.put('L',40);
-          romanToInt.put('C',50);
-          romanToInt.put('D',100);
-          romanToInt.put('M',500);
-         
+    void adddingValues() {
+        romanToInt.put('I', 1);
+        romanToInt.put('V', 5);
+        romanToInt.put('X', 10);
+        romanToInt.put('L', 40);
+        romanToInt.put('C', 50);
+        romanToInt.put('D', 100);
+        romanToInt.put('M', 500);
+
     }
-    
+
 }
+
+// import java.util.HashMap;
+
+// public class ElementCount {
+// public static void main(String[] args) {
+// int[] array = {1, 2, 3, 4, 1, 2, 3, 4, 5, 1};
+
+// // Create a HashMap to store the element count
+// HashMap<Integer, Integer> elementCountMap = new HashMap<>();
+
+// // Iterate through the array
+// for (int element : array) {
+// // Check if the element is already in the map
+// if (elementCountMap.containsKey(element)) {
+// // If yes, increment the count
+// elementCountMap.put(element, elementCountMap.get(element) + 1);
+// } else {
+// // If not, add the element to the map with count 1
+// elementCountMap.put(element, 1);
+// }
+// }
+
+// // Print the element count
+// for (int key : elementCountMap.keySet()) {
+// System.out.println("Element " + key + " occurs " + elementCountMap.get(key) +
+// " times.");
+// }
+// }
+// }
--- src/oddEVEN/SubString.java
+++ src/oddEVEN/SubString.java
@@ -1,32 +1,120 @@
 package oddEVEN;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class SubString {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    ArrayList<String> ans = new ArrayList<>();
-    ans = subString("", "abc");
-    System.out.println(ans);
+        // ArrayList<String> ans = new ArrayList<>();
+        // ans = subString("", "abc");
+        // System.out.println(ans);*
 
-     
-}
-
- 
- static ArrayList<String>  subString(String p , String up){
-    if(up.isEmpty()){
-        ArrayList<String> list = new ArrayList<>();
-        list.add(p);
-        return list;
+         int arr[] =  {3,0,1};
+        int ans = missingNumber(arr);
+        System.out.println(ans);
     }
- char ch = up.charAt(0);
- ArrayList<String> left = subString(p+ch, up.substring(1));
- ArrayList<String> right = subString(p, up.substring(1));
+    public static int missingNumber(int[] nums) {
+        int actualSum = actualSum(nums);
+        int incorrectSum = Arrays.stream(nums).sum();
 
- left.addAll(right);
- return left;
+        return actualSum - incorrectSum;
+    }
 
- }
-     
+    static int  actualSum(int arr[]) {
+        int n = arr.length;
+        return n * (n + 1) / 2;
+    }
+    List<String> subStringg(String p, String up) {
+
+        // base condtion
+        if (up.isEmpty()) {
+
+            ArrayList<String> ans = new ArrayList<>();
+            ans.add(p);
+            return ans;
+        }
+
+        // take it
+
+        List<String> left = subString(p + up.charAt(0), up.substring(1));
+
+        // ignore it
+        List<String> right = subString(p, up.substring(1));
+         left.addAll(right);
+         return left;
+    }
+
+    // prpgram to remove a from the given substring
+    static void removeA(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            removeA(p, up.substring(1));
+        } else {
+            removeA(p + ch, up.substring(1));
+        }
+    }
+
+    static String removeCharWithTwoParameters(String p, String up) {
+
+        if (up.isEmpty()) {
+            return p;
+        }
+
+        if (!up.startsWith("a")) {
+            return removeCharWithTwoParameters(p + up.charAt(0), up.substring(1));
+        } else {
+            return removeCharWithTwoParameters(p, up.substring(1));
+        }
+    }
+
+    static String apple(String p) {
+
+        if (p.isEmpty()) {
+            return "";
+        }
+
+        if (p.startsWith("apple")) {
+            return apple(p.substring(5));
+        } else {
+            return p.charAt(0) + apple(p.substring(1));
+        }
+
+    }
+
+    static String removeA(String a) {
+        if (a.isEmpty()) {
+            return "";
+        }
+
+        if (a.startsWith("a")) {
+            return removeA(a.substring(1));
+        } else {
+            return a.charAt(0) + removeA(a.substring(1));
+        }
+
+    }
+
+    static ArrayList<String> subString(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subString(p + ch, up.substring(1));
+        ArrayList<String> right = subString(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+
+    }
+
 }
