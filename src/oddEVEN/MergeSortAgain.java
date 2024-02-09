93c63aafac2bdaedb5fb99bc76019cd03a0b43a6
ArunKashyap <arunbhagat132@gmail.com>
2024-02-09 18:25:29 +0530

    ..

--- src/oddEVEN/MergeSortAgain.java
+++ src/oddEVEN/MergeSortAgain.java
@@ -1,34 +1,35 @@
+
+
+
 package oddEVEN;
 
 import java.util.Arrays;
 
 class MergeSortAgain {
-
     public static void main(String[] args) {
-        int[] arr = { 4, 3, 2, 1 };
+
+        int[] arr = { 7, 6, 5, 4, 3, 2, 1 };
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
 
     }
 
     static void quickSort(int[] arr, int low, int high) {
-
-        // this low and high are basically for the outer termination and to now in which
-        // part we are going to apply the sorting
         if (low > high) {
             return;
         }
 
         int s = low;
         int e = high;
-        int pivot = s + (e - s) / 2;
-        // basically this s and e are used for swapping and we are checking a violation
-        // with that while loop
+        int mid = s + (e - s) / 2;
+
+        int pivot = arr[mid];
+
         while (s <= e) {
-            while (arr[s] < arr[pivot]) {
+            while (arr[s] < pivot) {
                 s++;
             }
-            while (arr[e] > arr[pivot]) {
+            while (arr[e] > pivot) {
                 e--;
             }
 
@@ -43,6 +44,6 @@ class MergeSortAgain {
 
         quickSort(arr, low, e);
         quickSort(arr, s, high);
-    }
 
+    }
 }
