416a6844e53ecaedd6c8008ba0b1afc44ec1f878
ArunKashyap <arunbhagat132@gmail.com>
2024-02-28 19:54:12 +0530

    patterns

--- src/oddEVEN/revision.java
+++ src/oddEVEN/revision.java
@@ -1,14 +1,25 @@
 package oddEVEN;
-import java.util.Arrays;
 
 public class revision {
 
     public static void main(String[] args) {
+        pattern(5);
+    }
 
-        int[] arr = { 10, 1,4, 5, 6, 7, 8, 9, 2, 3,  };
+    static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
 
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
+            for (int k = 0; k < n * 2 - 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern(int n) {
 
     }
 
@@ -57,8 +68,50 @@ public class revision {
 
     }
 
-     
-    
+    // 5, 4, 3, 2, 1
 
+    // first select the max element and place at their respective position
+
+    static void cyclicSort(int[] arr) {
+
+        int i = 0;
+        int h = arr.length;
+
+        while (i < h) {
+            if (arr[i] == i + 1) {
+                i++;
+            } else {
+
+                arr[i] = i + 1;
+            }
+        }
+
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            int maxIndex = getMax(arr, j, arr.length - 1 - i);
+            swap(arr, maxIndex, arr.length - 1 - i);
+        }
+    }
+
+    static int getMax(int arr[], int l, int h) {
+        int maxIndex = 0;
+        int max = Integer.MIN_VALUE;
+        while (l < h) {
+            if (arr[l] > max) {
+                max = arr[l];
+                maxIndex = l;
+            }
+            l++;
+        }
+        return maxIndex;
+    }
+
+    static void swap(int[] arr, int firstIndex, int lastIndex) {
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[lastIndex];
+        arr[lastIndex] = temp;
+    }
 
 }
